2a2d72650d91d3749f7984bcdb3389b295e8cc6f
syky <sergiy.kucheryavyy@gft.com>
2023-07-23 17:47:49 +0200

    Init commit

---
+++ delete_tables.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+conn = sqlite3.connect('my_data.db')
+c = conn.cursor()
+
+# delete tables
+c.execute('''DROP TABLE test''')
+c.execute('''DROP TABLE ideal''')
+c.execute('''DROP TABLE train''')
